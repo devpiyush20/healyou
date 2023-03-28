44764ced5b9682b4149fd10a7d7c9b63d081b15f
Vimal Mishra <skk180509@gmail.com>
2023-03-28 20:44:53 +0530

    vim28032044

--- src/App.js
+++ src/App.js
@@ -17,7 +17,41 @@ import Services from './component/Services';
 function App() {
   return (
     <>
+                <Switch>
+<React.Fragment>
+   {/* <NavBar/> */}
+             <Route exact path='/' >
+
+   <Banner/>
+             </Route>
+             <Route exact path='/bookappointment' >
+
+              <BookForm/>
+                        </Route>
+                        <Route exact path='/bookbed' >
+
+              <BedForm/>
+                        </Route>
+             <Route exact path='/hospital' >
+             <HospitalCatalog/>
+             </Route>
+             <Route exact path='/pathalogy' >
+              <Pathalogy/>
+             </Route>
+             <Route exact path='/login' >
+    <Login/>
+             </Route>
+             <Route exact path='/register' >
+    <Register/>
+             </Route>
+             <Route exact path='/services' >
     <Services/>
+             </Route>
+
+
+
+</React.Fragment>
+</Switch>
     </>
   );
 }
