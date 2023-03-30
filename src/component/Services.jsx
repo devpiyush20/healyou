9784f2f9ce7f11a0a59b473cb2629c29244f0966
Vimal Mishra <skk180509@gmail.com>
2023-03-30 15:03:55 +0530

    functions

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../image/reactangle.png'
 import arrow1 from '../image/arrow1.png'
 import arrow2 from '../image/arrow2.png'
@@ -9,6 +9,15 @@ import BedCard from './BedCard'
 
  
 const Services = () => {
+  const reviews = [
+    "Lorem vimal ipsum dolor sit amet consectetur adipisicing elit. Quae molestiae impedit dignissimos quisquam culpa eius assumenda ut sapiente, pariatur debitis sequi eum adipisci dolor voluptatibus numquam ab maxime placeat aliquam.",
+    "Lorem raghav ipsum dolor sit amet consectetur adipisicing elit. Quae molestiae impedit dignissimos quisquam culpa eius assumenda ut sapiente, pariatur debitis sequi eum adipisci dolor voluptatibus numquam ab maxime placeat aliquam",
+    "Lorem piyush dolor sit amet consectetur adipisicing elit. Quae molestiae impedit dignissimos quisquam culpa eius assumenda ut sapiente, pariatur debitis sequi eum adipisci dolor voluptatibus numquam ab maxime placeat aliquam",
+    "Lorem neel dolor sit amet consectetur adipisicing elit. Quae molestiae impedit dignissimos quisquam culpa eius assumenda ut sapiente, pariatur debitis sequi eum adipisci dolor voluptatibus numquam ab maxime placeat aliquam",
+    "mera dengu hua is app se"
+  ]
+  const [idx,setIdx]=useState(0);
+  const [rw,setRw]=useState(reviews[idx]);
 
   const docData=[
     {
@@ -66,13 +75,34 @@ const Services = () => {
         }
     
   ]
+ 
+ const prev = ()=>{
+  if(idx === 0)
+  {
+    setIdx(reviews.length - 1);
+  }
+  else{
+    setIdx(idx - 1);
+  }
+  setRw(reviews[idx]);
+ }
+ const next = ()=>{
+  if(idx === reviews.length - 1)
+  {
+    setIdx(0);
+  }
+  else{
+    setIdx(idx + 1);
+  }
+  setRw(reviews[idx]);
+ }
   return (
     <>
      <div className="about">
       <div className="review">
       <img src={logo} alt="" />
-     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem ipsam, tempora adipisci magnam voluptate dolore. Praesentium a tempore error consectetur officiis rem iure! Explicabo, fuga iusto culpa maiores iste repellendus!</p>
-     <span className="continue-button"> <img src={arrow1} alt="arrow1" /> Customer Review <img src={arrow2} alt="arrow2" /> </span>
+     <p>{rw}</p>
+     <span className="continue-button"> <img src={arrow1} alt="arrow1" onClick={()=>{prev()}}/> Customer Review <img src={arrow2} alt="arrow2"  onClick={()=>{next()}}/> </span>
      </div>
       </div>
     
