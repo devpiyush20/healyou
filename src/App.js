9b7e9651971f6786ee37c9c0544f3aada07ec229
Vimal Mishra <skk180509@gmail.com>
2023-03-30 14:01:13 +0530

    something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import './App.css'
-import NavBar from './component/NavBar'
-import Banner from './component/Banner'
-import Card from './component/Card'
-import Catlog from './component/Catlog'
-import HospitalCatalog from './component/HospitalCatalog'
-import Pathalogy from './component/Pathalogy'
-import Login from './component/Login'
-import Navbar2 from './component/Navbar2'
+import './App.css';
+import Banner from './component/Banner';
+import HospitalCatalog from './component/HospitalCatalog';
+import Pathalogy from './component/Pathalogy';
+import Login from './component/Login';
+import {Switch,Route } from 'react-router-dom';
+import React from 'react';
+import Navbar2 from './component/Navbar2';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar2 />
-      {/* <NavBar /> */}
-      <Banner />
-      {/* <Catlog /> */}
-      {/* <HospitalCatalog /> */}
-      {/* <Pathalogy /> */}
-      {/* <Login /> */}
-    </div>
-  )
+    <>
+                <Switch>
+<React.Fragment>
+             <Route exact path='/' >
+   <Navbar2/>
+
+   <Banner/>
+             </Route>
+            
+             <Route exact path='/hospital' >
+             <HospitalCatalog/>
+             </Route>
+             <Route exact path='/pathalogy' >
+              <Pathalogy/>
+             </Route>
+             <Route exact path='/login' >
+    <Login/>
+             </Route>
+             
+           
+            
+
+
+
+</React.Fragment>
+</Switch>
+    </>
+  );
 }
 
-export default App
+export default App;
